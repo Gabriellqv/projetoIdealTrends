e58b495cc76f039d35e902ba9ddaa88f292e36be
Gabriellqv <gabriellqv@gmail.com>
2024-02-01 20:20:56 -0300

    initial commit

---
+++ assets/js/functions.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $("#slider").nivoSlider({
+        effect: "random",
+        animSpeed: 300,
+        pauseTime: 6000,
+        directionNav: true,
+        manualAdvance: false,
+        prevText: "<",
+        nextText: ">",
+    });
+});
